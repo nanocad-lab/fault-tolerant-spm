ebf1a73f78e47ac11c1aeb389ec3e98eea90a649
albertgfliu <albertgfliu@gmail.com>
2016-05-14 02:02:29 -0700

    Add address generator, 0x parsing not working

--- main.cpp
+++ main.cpp
@@ -126,9 +126,9 @@ main(int argc, char *argv[])
         string address_str = address_buff;
         printf("%s\n", address_str.c_str());
         unsigned int numeric_address = (unsigned int) stoul(address_str, nullptr, 16);
-
+	printf("0x%08x\n", numeric_address);
         if(numeric_address < SRAM_START || numeric_address > (SRAM_START + memory_size)){
-            fprintf(stderr, "The address %s is not within SRAM range", address_str.c_str());
+            fprintf(stderr, "The address %s is not within SRAM range\n", address_str.c_str());
             continue;
         }
 
